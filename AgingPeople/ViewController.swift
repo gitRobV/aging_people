46ec938a5fb9956c4f3799fad0669174b102a774
Robert Villarreal <me@robertv.co>
2017-07-10 11:34:09 -0700

    initial/Final commit

--- AgingPeople/ViewController.swift
+++ AgingPeople/ViewController.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var personTableView: UITableView!
 
+    
+    var personList = ["Olivia", "Jude", "Jacob", "Adrian", "Donovan", "Sophia", "Issac", "Angel", "GiGi", "Enzo", "Cooper", "Baby Adolfo"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return personList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = personList[indexPath.row]
+        cell.detailTextLabel?.text = String(randomAge())
+        return cell
+    }
+    
+    func randomAge() -> Int {
+        return Int(arc4random_uniform(91) + 5)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        personTableView.dataSource = self
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
 
 
